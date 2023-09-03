1589a7c564336094240dda5851ff40ec13079f3e
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-09-03 12:12:29 -0500

    more selenium...

--- day-48/interaction.py
+++ day-48/interaction.py
@@ -1,10 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-article_count_el = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-article_count = article_count_el.text
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("http://secure-retreat-92358.herokuapp.com/")
 
-print(article_count)
+form_fname = driver.find_element(By.NAME, "fName")
+form_lname = driver.find_element(By.NAME, "lName")
+form_email = driver.find_element(By.NAME, "email")
+form_btn = driver.find_element(By.TAG_NAME, "button")
+
+form_fname.send_keys("Kyle")
+form_lname.send_keys("Will")
+form_email.send_keys("email@example.com")
+form_btn.send_keys(Keys.ENTER)
+
+
+
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# article_count_el = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# print(article_count_el.text)
+
+# all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+# all_portals.click()
+
+# search = driver.find_element(By.NAME, "search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+
